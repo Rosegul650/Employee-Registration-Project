08be131d9875e6c0e09a467f9fda089fd696cbe4
Rosegul650 <121529555+Rosegul650@users.noreply.github.com>
2024-04-16 16:53:48 -0500

    Employee Registration project

---
+++ src/main/java/com/cydeo/EmployeeRegistretionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistretionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegistretionProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cydeo/model/Employee.java
@@ -0,0 +1,45 @@
+package com.cydeo.model;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+//    @NotNull    --->field should not be null
+//    @NotBlank   ---->fileld should not be "      "
+//    @NotEmpty  --->filed should not be " "
+
+//    @NotNull    -->@NotNull
+//    @NotBlank   -->NotNull + @NotEmpty + @NotBlank
+//    @NotEmpty   -->@NotNull +@NotEmpty
+
+    @NotBlank
+    @Size(max = 12, min = 3)
+    private String firstName;
+    private String lastName;
+    @NotNull
+    //Thymeleaf -->yyyy-MM-dd,   but LocalDate---->mm-dd-yyyy
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+
+    @NotNull
+    @Email
+    private String email;
+    @NotBlank
+
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+
+
+}
